2328487292617bed5e517ddb8ab6c2d25ffb9bf4
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-07-24 12:44:16 -0300

    finish Games: Rock Paper Scissors exercise

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -3,17 +3,20 @@ defmodule Games.GuessingGame do
   def play(number, 0), do: "You lose! the answer was #{number}"
 
   def play(number, max_attempts) do
-    input_number = String.trim(IO.gets "Guess a number between 1 and 10: ")
+    input_number = String.trim(IO.gets("Guess a number between 1 and 10: "))
     integer_number = String.to_integer(input_number)
 
     cond do
       integer_number < number ->
-        IO.puts "Too Low!"
+        IO.puts("Too Low!")
         play(number, max_attempts - 1)
+
       integer_number > number ->
-        IO.puts "Too High!"
+        IO.puts("Too High!")
         play(number, max_attempts - 1)
-      true -> "Correct!"
+
+      true ->
+        "Correct!"
     end
   end
 
---
+++ lib/rock_paper_scissors.ex
@@ -0,0 +1,29 @@
+defmodule Games.RockPaperScissors do
+  @moduledoc """
+  Rock, Paper, Scissors game
+  """
+
+  @doc """
+  Play the game
+  """
+  @spec play() :: String.t()
+  def play() do
+    ai_answer = Enum.random(["rock", "paper", "scissors"])
+    answer = String.trim(IO.gets("Choose rock, paper, or scissors: "))
+
+    cond do
+      answer == ai_answer -> "It's a tie!"
+      beats?(answer, ai_answer) -> "You win! #{answer} beats #{ai_answer}."
+      true -> "You lose! #{ai_answer} beats #{answer}."
+    end
+  end
+
+  @spec beats?(String.t(), String.t()) :: boolean()
+  defp beats?(player_answer, ai_answer) do
+    {player_answer, ai_answer} in [
+      {"rock", "scissors"},
+      {"scissors", "paper"},
+      {"paper", "rock"}
+    ]
+  end
+end
