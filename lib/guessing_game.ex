72fec857dfd6d80a23d7d2c31fcdbf4927df28f7
Rodrigo Santiago <rodrigosantiag@gmail.com>
2024-07-24 12:47:14 -0300

    Add documentation to Games.GuessingGame module

--- lib/guessing_game.ex
+++ lib/guessing_game.ex
@@ -1,25 +1,36 @@
 defmodule Games.GuessingGame do
-  def play(), do: play(generate_number(), 5)
-  def play(number, 0), do: "You lose! the answer was #{number}"
+  @moduledoc """
+  Number guessing game
+  """
 
-  def play(number, max_attempts) do
+  @doc """
+  Play the game
+  """
+  @spec play() :: String.t()
+  def play(), do: do_play(generate_number(), 5)
+
+  @spec do_play(integer(), integer()) :: String.t()
+  defp do_play(number, 0), do: "You lose! the answer was #{number}"
+
+  defp do_play(number, max_attempts) do
     input_number = String.trim(IO.gets("Guess a number between 1 and 10: "))
     integer_number = String.to_integer(input_number)
 
     cond do
       integer_number < number ->
         IO.puts("Too Low!")
-        play(number, max_attempts - 1)
+        do_play(number, max_attempts - 1)
 
       integer_number > number ->
         IO.puts("Too High!")
-        play(number, max_attempts - 1)
+        do_play(number, max_attempts - 1)
 
       true ->
         "Correct!"
     end
   end
 
+  @spec generate_number() :: integer()
   defp generate_number do
     Enum.random(1..10)
   end
